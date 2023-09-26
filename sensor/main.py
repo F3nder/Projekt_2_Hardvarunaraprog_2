cda493ea16a1ea0afa8df365f4c69739bda14a1a
Linus Oskarsson <linus-oskarsson@hotmail.com>
2023-09-26 12:25:34 +0200

    Moved config.json to sensor folder main.py: loads the config.json file and sets up the pin and intervals accordingly then prints the temp.

--- sensor/main.py
+++ sensor/main.py
@@ -1,1 +1,27 @@
 #main.py
+import machine, onewire, ds18x20, time
+import ujson
+
+with open("config.json") as conf:
+    data = ujson.load(conf) 
+    
+pin = data.get("pin", "Unknown")
+interval = data.get("interval", "Unknown")
+# print(pin)      #debug
+# print(interval) #debug
+
+
+ds_pin = machine.Pin(pin)
+ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
+
+roms = ds_sensor.scan()
+
+if not roms:
+    raise RuntimeError("Found no DS18b20")
+    
+while True:
+  ds_sensor.convert_temp()
+  time.sleep_ms(interval*10)
+  for rom in roms:
+    print(ds_sensor.read_temp(rom))
+  time.sleep(1)
--- sensor/temp2.py
+++ sensor/temp2.py
@@ -1,6 +1,17 @@
+#main.py
 import machine, onewire, ds18x20, time
- 
-ds_pin = machine.Pin(16)
+import ujson
+
+with open("config.json") as conf:
+    data = ujson.load(conf) 
+    
+pin = data.get("pin", "Unknown")
+interval = data.get("interval", "Unknown")
+print(pin)
+print(interval)
+
+
+ds_pin = machine.Pin(pin)
 ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
 
 roms = ds_sensor.scan()
@@ -10,7 +21,24 @@ if not roms:
     
 while True:
   ds_sensor.convert_temp()
-  time.sleep_ms(750)
+  time.sleep_ms(interval*10)
   for rom in roms:
     print(ds_sensor.read_temp(rom))
   time.sleep(1)
+
+# import machine, onewire, ds18x20, time
+ 
+# ds_pin = machine.Pin(16)
+# ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
+
+# roms = ds_sensor.scan()
+
+# if not roms:
+#     raise RuntimeError("Found no DS18b20")
+    
+# while True:
+#   ds_sensor.convert_temp()
+#   time.sleep_ms(750)
+#   for rom in roms:
+#     print(ds_sensor.read_temp(rom))
+#   time.sleep(1)
