089443aa40fdc935873ddbf22dbaa59afe23367c
Linus Oskarsson <linus-oskarsson@hotmail.com>
2023-10-11 13:36:36 +0200

    added comments

--- hub/main.py
+++ hub/main.py
@@ -4,38 +4,49 @@ import json
 import time
 import struct
 
+# Read configuration from 'config.json' file
 with open("hub/config.json") as conf:
     data = json.load(conf) 
+
+# Get configuration values or use default values if not found
 name = data.get("name", "Unknown")
 broker = data.get("broker", "Unknown")
 topic = data.get("topic", "Unknown")
 
-
-
+# Initialize an MQTT client and connect to the broker
 client = mqtt.Client()
 client.connect(broker, port=1883, keepalive=60)
 client.loop_start()
 
+# Continuously read data from a serial port and publish it to MQTT
 while True:
+    # Establish a serial connection to the specified port with a baud rate of 115200
     with serial.Serial('COM5', 115200, timeout=1) as ser:
         line = ser.readline()
         timestamp = time.time()
         timestamp_int = int(timestamp)
+
         if len(line) > 0:
             line_split = line.split()
             pico_id = line_split[0]
             temp_id = line_split[1]
             temp = line_split[2]
             
+            # Decode bytes to UTF-8 strings
             pico_id_decode = pico_id.decode('utf-8')
             temp_id_decode = temp_id.decode('utf-8')
-
             temp_decode = temp.decode('utf-8')
+            
+            # Converts temp
             temp_float = float(temp_decode)
             temp_round = int(temp_float * 1000)
 
+             # Pack the timestamp and temperature data into a binary payload
             payload_ = struct.pack('>Ii', timestamp_int, temp_round)
+
+             # Construct the MQTT topic based on configuration and received data
             long_topic = f"{topic}/{name}/{pico_id_decode}/{temp_id_decode}"
 
+            # Publish the payload to the MQTT broker with QoS 1
             msg = client.publish(long_topic, payload=payload_, qos=1)
             msg.wait_for_publish()
--- sensor/main.py
+++ sensor/main.py
@@ -1,30 +1,38 @@
 #main.py
 import machine, onewire, ds18x20, time, ujson
 
+# Read the configuration from the "config.json" file
 with open("config.json") as conf:
     data = ujson.load(conf) 
     
+# Get the 'pin' and 'interval' values from the configuration, or use default values if not found
 pin = data.get("pin", "Unknown")
 interval = data.get("interval", "Unknown")
 
+# Function to get the unique ID of the Pico board and convert it to a hexadecimal string
 def pico_id_get():
   pico_id = machine.unique_id()
   pico_id_hex = pico_id.hex()
   return pico_id_hex
 
+# Function to convert the ROM (unique identifier) of a DS18b20 temperature sensor to a hexadecimal string
 def temp_id_get(rom):
    temp_id = rom
    temp_id_hex = temp_id.hex()
    return temp_id_hex
 
+# Initialize the OneWire bus and DS18X20 sensor using the specified pin
 ds_pin = machine.Pin(pin)
 ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
 
+# Scan for DS18b20 temperature sensors on the OneWire bus
 roms = ds_sensor.scan()
 
+# Raise an error if no DS18b20 sensors are found
 if not roms:
     raise RuntimeError("Found no DS18b20")
     
+# Main loop: continuously read temperature data from the DS18b20 sensors
 while True:
   ds_sensor.convert_temp()
   time.sleep_ms(750)
